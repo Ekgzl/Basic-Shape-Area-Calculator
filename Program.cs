0e6aadf171d54b0cc58b8c2b3e4fe46dd02e6221
Ekgzl <105228606+Ekgzl@users.noreply.github.com>
2024-01-15 10:18:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ShapeAreaCalculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        float pi = 3.14f;
+        float vertical, horizontal;
+        char isYes;
+        do
+        {
+            char switchKey;
+            do
+            {
+                Console.WriteLine("Please enter shape: ");
+                Console.WriteLine("1- Rectangle(r)\n2- Triangle(t)\n3-Circle(c)");
+                switchKey = Console.ReadLine()[0];
+            } while (switchKey != 'R' && switchKey != 'r' && switchKey != 'T'&& switchKey != 't'&& switchKey != 'c' && switchKey != 'C');
+            
+            
+            switch (switchKey)
+            {
+                case 't':
+                case 'T': 
+                    Console.WriteLine("Please provide height: ");
+                    vertical = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please provide basement of triangle: ");
+                    horizontal = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(vertical * horizontal /2.0f);
+                    break;
+                case 'R':
+                case 'r':
+                    Console.WriteLine("Please provide height: ");
+                    vertical = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please provide length: ");
+                    horizontal = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(vertical * horizontal);
+                    break;
+                case 'C':
+                case 'c':
+                    Console.WriteLine("Please provide radius: ");
+                    vertical = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(vertical*pi*vertical);
+                    break;
+            }
+
+            System.Threading.Thread.Sleep(2500);
+            Console.Clear();
+            do
+            {
+                Console.WriteLine("Do you want to play again?:[Y]/[N] ");
+                isYes = Console.ReadLine()[0];
+            } while (isYes != 'y' && isYes != 'Y' && isYes != 'N' && isYes != 'n');
+            
+        } while (isYes == 'y' || isYes == 'Y');
+    }
+}
